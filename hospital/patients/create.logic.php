afa67a9d9444df212f5a4442acbcdc738e0911df
rodyict <rody.d.ros@gmail.com>
2016-02-15 11:08:21 +0100

    Create helemaal functioneel
    Clienten en Species kunnen nu doormiddel van een dropdown worden
    gekozen.

--- hospital/patients/create.logic.php
+++ hospital/patients/create.logic.php
@@ -4,17 +4,30 @@
 		
 		// Prepare data for insertion
 		$name = $db->escape_string($_POST["name"]);
-		$species = $db->escape_string($_POST["species"]);
-		$status = $db->escape_string($_POST["status"]);
 		$gender = $db->escape_string($_POST["gender"]);
+		$status = $db->escape_string($_POST["status"]);
+		$clientid = $db->escape_string($_POST["clientid"]);
+		$speciesid = $db->escape_string($_POST["speciesid"]);
 
 		// Prepare query and execute
-		$query = "insert into patient (name, species, status, gender) values ('$name','$species','$status','$gender')";
+		$query = "insert into patient (name, gender, status, clientid,speciesid) values ('$name', '$gender', '$status', $clientid, $speciesid)";
+		echo $query;
 		$result = $db->query($query);
 	
-    // Tell the browser to go back to the index page.
-    header("Location: ./");
-    exit();
+	    // Tell the browser to go back to the index page.
+	    header("Location: ./");
+    	exit();
+    elseif ($_SERVER["REQUEST_METHOD"] == "GET"):
+    		$db = new mysqli('localhost','root','','hospital');
+			
+			$query2 = "select * from client";
+			$query3 = "select * from species";
+
+			$result2 = $db->query($query2);
+			$result3 = $db->query($query3);
+
+			$clients = $result2->fetch_all(MYSQLI_ASSOC);	
+			$species = $result3->fetch_all(MYSQLI_ASSOC);
 	endif;
 
 ?>
--- hospital/patients/create.php
+++ hospital/patients/create.php
@@ -9,12 +9,30 @@
 			<input type="text" id="name" name="name">
 		</div>
 		<div>
-			<label for="name">Species:</label>
-			<input type="text" id="species" name="species">
+			<label for="gender">Gender:</label><br>
+			<input type="radio" name="gender" value="male" checked> Male<br>
+  			<input type="radio" name="gender" value="female"> Female<br>
+		</div>
+
+		<div>
+			<label for="name">Status:</label>
+			<textarea id="status" name="status"></textarea>
 		</div>
 		<div>
-			<label for="name">Species:</label>
-			<textarea id="status" name="status"></textarea>
+			<label for="species">Species:</label>
+			<select name="speciesid" id="specie">
+				<?php foreach($species as $specie): ?>
+					<option value="<?= $specie['id']; ?>" <?php if ($specie['id'] == $specie['id']) { echo "selected"; } ?>><?= $specie['soort']; ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+		<div>
+			<label for="client">Client:</label>
+			<select name="clientid" id="client">
+				<?php foreach($clients as $client): ?>
+					<option value="<?= $client['id']; ?>" <?php if ($client['id'] == $client['id']) { echo "selected"; } ?>><?= $client['name']; ?></option>
+				<?php endforeach; ?>
+			</select>
 		</div>
 		<div>
 			<label></label>
